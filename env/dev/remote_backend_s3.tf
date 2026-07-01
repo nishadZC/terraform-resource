9d9bcd23feceee84e5362c3876b870e0c06edbf3
nishadZC <vinayak.nishad@zapcg.com>
2026-07-01 14:33:16 +0530

    Added env for remote backup terrafom file

--- env/dev/remote_backend_s3.tf
+++ env/dev/remote_backend_s3.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-jenkins-remote-state-bucket-apsouth1"
-    key    = "devops-project-1/jenkins/terraform.tfstate"
+    bucket = "${var.environment}-eventify-bucket-apsouth1"
+    key    = "${var.environment}/eventify/terraform.tfstate"
     region = "ap-south-1"
   }
 }
